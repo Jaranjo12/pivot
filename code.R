afb0252becf60bb94d1930af6c05a12915e758c6
sporella <sporella@uc.cl>
2020-08-31 03:48:29 -0400

    live coding

--- code.R
+++ code.R
@@ -1,8 +1,8 @@
+# RStudio Certification - Teaching Exam
+# Stephanie Orellana
+
+
+# Librerías ---------------------------------------------------------------
+
 library(tidyverse)
 library(datos)
-
-pinguinos <- pinguinos
-
-pivot_longer(data = pinguinos, cols = largo_pico_mm:masa_corporal_g)
-pivot_longer(data = pinguinos, cols = largo_pico_mm:masa_corporal_g, names_to = "variables", values_to = "valor")
-pivot_longer(data = pinguinos, cols = largo_pico_mm:masa_corporal_g, names_to = c("variables", "unidad"), values_to = "valor", names_pattern = "(.*_.*)_(.*)")
