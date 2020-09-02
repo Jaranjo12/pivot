621a44d2fced19fd671469c379cd4a5f31939855
sporella <sporella@uc.cl>
2020-09-02 13:22:50 -0400

    live coding

--- code.R
+++ code.R
@@ -6,3 +6,30 @@
 
 library(tidyverse)
 library(datos)
+
+pinguinos <- pinguinos
+
+pivot_longer(data = pinguinos, cols = largo_pico_mm:masa_corporal_g)
+
+pivot_longer(
+  data = pinguinos,
+  cols = largo_pico_mm:masa_corporal_g,
+  names_to = "variables",
+  values_to = "valor"
+)
+
+pivot_longer(
+  data = pinguinos,
+  cols = largo_pico_mm:masa_corporal_g,
+  names_to = "variables",
+  values_to = "valor",
+  names_pattern = "(.*_.*)_(.*)"
+)
+
+pivot_longer(
+  data = pinguinos,
+  cols = largo_pico_mm:masa_corporal_g,
+  names_to = c("variables", "unidad") ,
+  values_to = "valor",
+  names_pattern = "(.*_.*)_(.*)"
+)
